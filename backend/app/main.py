0b507b21829f483e7572170c5b50d00c58627a88
Artem <progerguru@gmail.com>
2025-04-19 13:00:31 +0300

    feat(users): add user views

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from contextlib import asynccontextmanager
 
 from core.models import db_helper
 from core.config import settings
+from api import router as api_router
 
 
 @asynccontextmanager
@@ -16,6 +17,7 @@ async def lifespan(lifespan_app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+app.include_router(api_router)
 
 if __name__ == "__main__":
     uvicorn.run(
