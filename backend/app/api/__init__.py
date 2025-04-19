7ef965e0ff2a5f906d94b4bd83f355ad87e0d4bc
mixelka75 <mix@yuuri.online>
2025-04-20 02:26:50 +0300

    я же вам запретил спать

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -5,6 +5,7 @@ from .auth import router as auth_router
 from .reviews import router as reviews_router
 from .faculties import router as faculties_router
 from .programs.views import router as program_route
+from .professors import router as professors_router  # Добавляем импорт
 
 router = APIRouter()
 
@@ -12,5 +13,6 @@ router.include_router(users_router)
 router.include_router(auth_router)
 router.include_router(reviews_router)
 router.include_router(faculties_router)
+router.include_router(professors_router)  # Добавляем регистрацию
 
 router.include_router(program_route)
