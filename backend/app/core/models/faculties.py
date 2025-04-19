84f70be657bf8663dbd0286f57c81dad4631b37b
Artem <progerguru@gmail.com>
2025-04-19 18:41:12 +0300

    feat(subjects): add subject model

--- backend/app/core/models/faculties.py
+++ backend/app/core/models/faculties.py
@@ -5,24 +5,18 @@ from .mixins.id_mixin import IDMixin
 from .mixins.timestamp_mixin import TimestampMixin
 
 
-class Faculty(IDMixin, TimestampMixin, Base):
+class Faculty(IDMixin, Base):
     """
     Модель SQLAlchemy, представляющая факультет.
 
     Атрибуты:
         id (int): Уникальный идентификатор (наследуется от IDMixin).
-        created_at (datetime): Дата создания (наследуется от TimestampMixin).
-        updated_at (datetime): Дата последнего обновления (наследуется от TimestampMixin).
         name (str): Название кафедры (до 150 символов).
         description (str): Описание кафедры.
-        faculty_id (int): Внешний ключ на факультет.
     """
 
     name: Mapped[str] = mapped_column(String(150), nullable=False, unique=True)
     description: Mapped[str] = mapped_column(String(500), nullable=True)
 
     def __repr__(self) -> str:
-        return (
-            f"<Faculty(id={self.id}, name='{self.name}', "
-            f"created_at='{self.created_at}')>"
-        )
+        return f"<Faculty(id={self.id}, name='{self.name}, description={self.description})>')>"
